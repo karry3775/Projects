59b4e719eb65978d98eca44c1a5b139a358a44c7
karry3775 <43415631+karry3775@users.noreply.github.com>
2019-01-15 13:34:59 -0600

    Add files via upload

---
+++ drive.py
@@ -0,0 +1,54 @@
+import socketio
+import eventlet
+from flask import Flask
+from keras.models import load_model
+import base64
+from io import BytesIO
+from PIL import Image
+import numpy as np
+import cv2
+
+sio =socketio.Server()
+app = Flask(__name__)
+speed_limit = 10
+
+def img_preprocess(img):
+  img = img[60:135 ,:,:] # to crop of the hood of the car and trees
+  img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV) #effective for nvidia model
+  img = cv2.GaussianBlur(img, (3,3), 0) #adding the gaussian blur
+  img = cv2.resize(img, (200,66)) #(200,66) matches with nvidia model
+  img = img/255 #normalization
+  return img
+
+@sio.on('telemetry')
+def telemetry(sid, data):
+    speed = float(data['speed'])
+    image = Image.open(BytesIO(base64.b64decode(data['image'])))
+    image = np.asarray(image)
+    image = img_preprocess(image)
+    image = np.array([image])
+    steering_angle = float( model.predict(image))
+    throttle = 1.0 - speed/speed_limit
+    print('{} {} {}'.format(steering_angle,throttle,speed))
+    send_control(steering_angle,1.0)
+
+
+
+
+@sio.on('connect') #three reserved keywords, connect, message, disconnect, although you can call it anything u like
+def connect(sid, environ):
+    print('Connected')
+    send_control(0,0)
+
+def send_control(steering_angle,throttle):
+    sio.emit('steer',data={
+        'steering_angle': steering_angle.__str__(),
+        'throttle' : throttle.__str__()
+
+    })
+
+
+if __name__ == '__main__':
+    model = load_model('model.h5')
+    app = socketio.Middleware(sio,app)
+    eventlet.wsgi.server(eventlet.listen(('',4567)),app) # (ip,port)
