2648e4cadfb9023be093dc903c83ca9d04a8d762
karry3775 <43415631+karry3775@users.noreply.github.com>
2019-01-25 09:13:33 -0600

    Update MobileVideofeed.py
    Added line detection algorithm

--- MobileVideofeed.py
+++ MobileVideofeed.py
@@ -12,14 +12,15 @@ import urllib
 import cv2
 import numpy as np
 
-url = "http://192.168.0.3:8080/shot.jpg" #replace with your url
+url = "http://192.168.0.3:8080/shot.jpg"
 
 def canny_edge(img):
-    gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     kernel = 5
     blur =  cv2.GaussianBlur(gray,(kernel,kernel),0)
-    canny_img = cv2.Canny(blur,10,200)
+    canny_img = cv2.Canny(blur,10,250,apertureSize=5,L2gradient=True) #10 and 100 are the lower and upper limit to be considered as an edge
     return canny_img
+
 
 while True:
     #get the repsonse from url
@@ -27,10 +28,37 @@ while True:
     #convert it into a numpy image
     imagenp = np.array(bytearray(img_resp.read()),dtype=np.uint8)
     #convert it to a cv2 image
-    img = cv2.imdecode(imagenp,-1)
+    frame = cv2.imdecode(imagenp,-1)
     # canny_edge detection
-    img = canny_edge(img)
+    edges = canny_edge(frame)
+
+    # Dilating the image
+    dilated = cv2.dilate(edges, np.ones((3,3), dtype=np.uint8))
+    # line detection using Normal HOUGH Transform
+    # lines = cv2.HoughLines(dilated,1,np.pi/180,250)
+    # # from the above line of code we get rho and theta values we need cartesian values
+    # if lines is not None:
+    #     for rho,theta in lines[0]:
+    #         x0 = rho*(np.cos(theta))
+    #         y0 = rho*(np.sin(theta))
+    #         x1 = int(x0 - 1000*(np.sin(theta)))
+    #         y1 = int(y0 + 1000*(np.cos(theta)))
+    #         x2 = int(x0 + 1000*(np.sin(theta)))
+    #         y2 = int(y0 - 1000*(np.cos(theta)))
+    #         cv2.line(frame,(x1,y1),(x2,y2),(0,255,0),3)
+
+    # lets use probabilistic HoughLinesP
+
+    minLineLength = 100
+    maxLineGap = 10
+    lines = cv2.HoughLinesP(dilated,1,np.pi/180,100,minLineLength,maxLineGap)
+    if lines is not None:
+        for x1,y1,x2,y2 in lines[0]:
+            cv2.line(frame,(x1,y1),(x2,y2),(0,255,0),2)
+
+
     #display image
-    cv2.imshow('frame_window',img)
+    cv2.imshow('frame_window',frame)
     if ord('q')==cv2.waitKey(10):
         exit(0)
+
